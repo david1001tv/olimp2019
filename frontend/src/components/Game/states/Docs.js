292ea2af25fc6f5d7b3a2301547b890653b48646
david1488tv <37955716+david1488tv@users.noreply.github.com>
2018-04-30 20:57:52 +0300

    Квест 1-2

---
+++ frontend/src/components/Game/states/Docs.js
@@ -0,0 +1,137 @@
+/* globals __DEV__ */
+import Phaser from 'phaser';
+
+
+export default class DocsState extends Phaser.State {
+    * gen() {
+        console.log(0);
+        console.log(this.camera);
+        window.CAMERA = this.camera;
+        // this.camera.scale.setTo(5, 5);
+        this.camera.x = 1128;
+        this.camera.y = 280;
+        this.game.add.tween(this.camera.scale).to({
+            x: 1,
+            y: 1,
+        }, 3000).start().onComplete.add(() => {
+            this.next();
+        });
+        this.game.camera.flash(0x000000, 3000, true);
+        yield;
+
+
+        /*this.game.add.tween(this.mom).to({
+            alpha: 1
+        }, 1500, Phaser.Easing.Cubic.In)
+        .start().onComplete.add(() => {
+            this.next();
+        });
+        yield;*/
+
+
+        /*this.game.displayDialogLine('Маман', 'Доброго ранку синку! Тобі вже прийшов лист з університету?');
+        yield;
+
+
+        this.game.displayDialogLine('Ви', 'Так, мамо');
+        yield;
+
+
+        this.game.displayDialogLine('Маман', 'Ти вже обрав спеціальність? Син моєї подруги програміст, вже на першому курсі він отримував 1000$ щомісяця.');
+        yield;
+
+
+        this.game.displayDialogLine('Ви', 'Добре, мамо');
+        yield;*/
+    }
+
+    init() {
+        this._gen = this.gen();
+    }
+
+    preload() {
+        this.load.image('bg', './assets/images/quest-bg-1-2.png');
+        this.load.image('door-closed', './assets/images/door-closed.png');
+        this.load.image('d-right', './assets/images/d-right.png');
+        this.load.image('sertificate', './assets/images/sertificate.png');
+        this.load.image('door-opened', './assets/images/door-opened.png');
+        this.load.image('passport', './assets/images/passport.png');
+        this.load.image('photos', './assets/images/photos.png');
+        this.load.image('warticket', './assets/images/warticket.png');
+        this.load.image('zno', './assets/images/zno.png');
+    }
+
+    create() {
+        let bg = this.game.add.image(0, 0, 'bg');
+        bg.height = this.game.width * bg.height / bg.width;
+        bg.width = this.game.width;
+
+        let door_closed = this.game.add.image(103, 234, 'door-closed');
+        door_closed.aspectRatio = door_closed.width / door_closed.height;
+        door_closed.height = 540;
+        door_closed.width = door_closed.aspectRatio * door_closed.height;
+        door_closed.alpha = 0;
+        this.door_closed = door_closed;
+
+        let door_opened = this.game.add.image(103, 234, 'door_opened');
+        door_opened.aspectRatio = door_opened.width / door_opened.height;
+        door_opened.height = 540;
+        door_opened.width = door_opened.aspectRatio * door_opened.height;
+        door_opened.alpha = 0;
+        this.door_opened = door_opened;
+
+        let sertificate = this.game.add.image(1430, 175, 'sertificate');
+        sertificate.aspectRatio = sertificate.width / sertificate.height;
+        sertificate.height = 120;
+        sertificate.width = sertificate.aspectRatio * sertificate.height;
+
+        let photos = this.game.add.image(1170, 525, 'photos');
+        photos.aspectRatio = photos.width / photos.height;
+        photos.height = 40;
+        photos.width = photos.aspectRatio * photos.height;
+
+        let warticket = this.game.add.image(1720, 340, 'warticket');
+        warticket.aspectRatio = warticket.width / warticket.height;
+        warticket.height = 40;
+        warticket.width = warticket.aspectRatio * warticket.height;
+
+        let zno = this.game.add.image(660, 783, 'zno');
+        zno.aspectRatio = zno.width / zno.height;
+        zno.height = 50;
+        zno.width = zno.aspectRatio * zno.height;
+
+        let passport = this.game.add.image(364, 435, 'passport');
+        passport.aspectRatio = passport.width / passport.height;
+        passport.height = 23.12345;
+        passport.width = passport.aspectRatio * passport.height;
+
+        let david = this.game.add.image(1300, 280, 'd-right');
+        david.aspectRatio = david.width / david.height;
+        david.height = 551;
+        david.width = david.aspectRatio * david.height;
+
+        // const bannerText = 'Phaser + ES6 + Webpack';
+        // let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
+        //     font: '40px Bangers',
+        //     fill: '#77BFA3',
+        //     smoothed: false
+        // });
+
+        // banner.padding.set(10, 16);
+        // banner.anchor.setTo(0.5);
+
+        this.stage.disableVisibilityChange = true;
+
+        this.next();
+    }
+
+    render() {
+        // if (__DEV__) {
+        //this.game.debug.spriteInfo(this.mom, 32, 32);
+        // }
+    }
+
+    next() {
+        this._gen.next();
+    }
+}
