2d11d253dedbece4e2f0e9d03fb8c2ccb7c95c99
david1488tv <37955716+david1488tv@users.noreply.github.com>
2018-05-05 14:00:51 +0300

    Конечная версия кроссворда + квеста-воды

--- frontend/src/components/Game/states/Boot.js
+++ frontend/src/components/Game/states/Boot.js
@@ -23,10 +23,10 @@ export default class BootState extends Phaser.State {
 
     render() {
         if (config.webfonts.length && this.fontsReady) {
-            this.state.start('Water_Market');
+            this.state.start('Bubble');
         }
         if (!config.webfonts.length) {
-            this.state.start('Water_Market');
+            this.state.start('Bubble');
         }
     }
 
