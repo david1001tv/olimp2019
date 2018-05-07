89b010e0f0e1b90ed1a590168776f81338304a29
cast98 <kostyawwe98@gmail.com>
2018-05-07 19:43:40 +0300

    небольшой фикс диалога в магазе

--- frontend/src/components/Game/states/SecondStage/WaterMarket.js
+++ frontend/src/components/Game/states/SecondStage/WaterMarket.js
@@ -94,7 +94,7 @@ export default class WaterState extends Phaser.State {
         this.testAPI.deleteText(thirdQuestion, answers);
         this.testAPI.destroyNote();
         console.log("grade:" + this.grade);
-        this.game.displayDialogLine('Продавець', 'Дякую! А ось як раз і ваша вода набралась. Прошу.', () => this.next());
+        setTimeout(() => this.game.displayDialogLine('Продавець', 'Дякую! А ось як раз і ваша вода набралась. Прошу.', () => this.next()), 100);
         let bottles = this.game.add.image(440, 380, 'bottles');
         smartSetHeight(bottles, 300);
         yield;
