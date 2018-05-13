accaa46e7f71414adf2c51f86afe8c16f3c5992b
david1488tv <37955716+david1488tv@users.noreply.github.com>
2018-05-13 19:53:39 +0300

    Фиксы в оценивании + новый фон

--- frontend/src/components/Game/states/SecondStage/Grades.js
+++ frontend/src/components/Game/states/SecondStage/Grades.js
@@ -9,7 +9,7 @@ export default class GradesState extends Phaser.State {
         системи.', () => this.next());
         yield;
         let att = this.history.find(e => e.state === 'Docs').score + this.history.find(e => e.state === 'Scanner').score;
-        this.game.add.text(650, 340, att, {
+        this.game.add.text(1020, 470, att, {
             font: "30px Pangolin",
         });
         //yield;
@@ -17,7 +17,7 @@ export default class GradesState extends Phaser.State {
         this.game.displayDialogLine('Федосова', 'Наступне в нас ЗНО з української мови.', () => this.next());
         yield;
         let ukr = this.history.find(e => e.state === 'Cross').score;
-        this.game.add.text(770, 410, ukr, {
+        this.game.add.text(1020, 530, ukr, {
             font: "30px Pangolin",
         });
        // yield;
@@ -25,7 +25,7 @@ export default class GradesState extends Phaser.State {
         this.game.displayDialogLine('Федосова', 'Далі математика.', () => this.next());
         yield;
         let math = this.history.find(e => e.state === 'WaterMarket').score;
-        this.game.add.text(705, 480, math, {
+        this.game.add.text(1020, 590, math, {
             font: "30px Pangolin",
         });
         //yield;
@@ -33,7 +33,7 @@ export default class GradesState extends Phaser.State {
         this.game.displayDialogLine('Федосова', 'Остання англійська мова.', () => this.next());
         yield;
         let eng = this.history.find(e => e.state === 'Translate').score;
-        this.game.add.text(695, 550, eng, {
+        this.game.add.text(1020, 650, eng, {
             font: "30px Pangolin",
         });
         //yield;
@@ -41,10 +41,20 @@ export default class GradesState extends Phaser.State {
         this.game.displayDialogLine('Федосова', 'Тепер ми можем побачити твій загальний рейтинг.', () => this.next());
         yield;
         let rait = (0.1 * att) + (0.4 * math) + (0.2 * ukr) + (0.3 * eng);
-        this.game.add.text(570, 620, rait, {
+        this.game.add.text(980, 710, rait, {
             font: "30px Pangolin",
         });
         //yield;
+        this.game.displayDialogLine('Федосова', 'Тепер підпис...', () => this.next());
+        yield;
+
+        this.game.add.tween(this.signature).to({
+            alpha: 1
+        }, 1500, Phaser.Easing.Cubic.InOut)
+            .start().onComplete.add(() => {
+            this.next();
+        });
+        yield;
 
         this.game.displayDialogLine('Федосова', 'Начебто усе. Зараз, поставлю печать.', () => this.next());
         yield;
@@ -80,10 +90,11 @@ export default class GradesState extends Phaser.State {
     }
 
     preload() {
-        this.load.image('bg', './assets/images/2-5 (audience)/bg-papers.png');
+        this.load.image('bg', './assets/images/2-5 (audience)/bg-papers-n.png');
         this.load.image('stamp', './assets/images/2-5 (audience)/stamp.png');
         this.load.image('hand-stamps', './assets/images/2-5 (audience)/hand-stamps.png');
         this.load.image('hand-wait', './assets/images/2-5 (audience)/hand-wait.png');
+        this.load.image('ok', './assets/images/2-1 (crossword)/ok.png');
     }
 
     create() {
@@ -97,37 +108,74 @@ export default class GradesState extends Phaser.State {
             this.next();
         });
         
-        this.game.add.text(800, 200, "ПДТУ", {
+        this.game.add.text(800, 220, "ПДТУ", {
             font: "50px Pangolin",
         });
-        this.game.add.text(450, 270, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at.", {
+        this.game.add.text(520, 300, "Це офіційний документ у якому будуть виставлені ваші \nбали ЗНО, \
+атестату і рейтинговий бал для вступу в ВНЗ", {
+            font: "30px Pangolin"
+        });
+        this.game.add.text(530, 415, "Найменування", {
+            font: "35px Pangolin",
+        });
+        this.game.add.text(810, 415, "Коефіцієнт", {
+            font: "35px Pangolin",
+        });
+        this.game.add.text(810, 470, "0,1", {
             font: "30px Pangolin",
         });
-        this.game.add.text(450, 340, "Бал аттестату: ", {
+        this.game.add.text(810, 530, "0,2", {
             font: "30px Pangolin",
         });
-        this.game.add.text(450, 410, "ЗНО з української мови: ", {
+        this.game.add.text(810, 590, "0,4", {
             font: "30px Pangolin",
         });
-        this.game.add.text(450, 480, "ЗНО з математики: ", {
+        this.game.add.text(810, 650, "0,3", {
             font: "30px Pangolin",
         });
-        this.game.add.text(450, 550, "ЗНО з англійської: ", {
+        this.game.add.text(1020, 415, "Бали", {
+            font: "35px Pangolin",
+        });
+        this.game.add.text(530, 470, "Бал аттестату", {
             font: "30px Pangolin",
         });
-        this.game.add.text(450, 620, "Рейтинг: ", {
+        this.game.add.text(530, 530, "Українська мова ", {
             font: "30px Pangolin",
+        });
+        this.game.add.text(530, 590, "Математика", {
+            font: "30px Pangolin",
+        });
+        this.game.add.text(530, 650, "Англійська мова", {
+            font: "30px Pangolin",
+        });
+        this.game.add.text(530, 710, "Рейтинг: ", {
+            font: "30px Pangolin",
+        });
+        this.game.add.text(500, 850, "Завідувач кафедрою", {
+            font: "30px Pangolin",
+        });
+        this.game.add.text(1040, 850, "І.В. Федосова", {
+            font: "30px Pangolin",
+        });
+        this.game.add.text(850, 895, "підпис", {
+            font: "15px Pangolin",
+        });
+        this.game.add.text(1110, 895, "П.І.Б.", {
+            font: "15px Pangolin",
         });
 
-        let stamp = this.game.add.sprite(1000, 2000, 'stamp');
+        this.signature = this.game.add.image(820, 800, 'ok');
+        smartSetHeight(this.signature, 100);
+        this.signature.alpha = 0;
+        let stamp = this.game.add.sprite(850, 2000, 'stamp');
         smartSetHeight(stamp, 200);
-        let handWait = this.game.add.sprite(1200, 2000, 'hand-wait');
+        let handWait = this.game.add.sprite(1050, 2000, 'hand-wait');
         smartSetHeight(handWait, 500);
-        let handStamps = this.game.add.sprite(990, 2000, 'hand-stamps');
+        let handStamps = this.game.add.sprite(870, 2000, 'hand-stamps');
         smartSetHeight(handStamps, 470);
 
         this.firstTween = this.game.add.tween(handWait).to( { y: 700 }, 1500);
-        this.secondTween = this.game.add.tween(handWait).to( { x: 1120, y: 770 }, 300);
+        this.secondTween = this.game.add.tween(handWait).to( { x: 1000, y: 770 }, 300);
         this.thirdTween = this.game.add.tween(handWait).to( { y: 2000 }, 10);
         this.fourTween = this.game.add.tween(handStamps).to( { y: 780 }, 10);
         this.fiveTween = this.game.add.tween(handStamps).to({}, 200);
@@ -135,7 +183,7 @@ export default class GradesState extends Phaser.State {
         this.sevenTween = this.game.add.tween(stamp).to( { y: 800 }, 10);
         this.eightTween = this.game.add.tween(handStamps).to( { y: 2000 }, 10);
         this.nineTween = this.game.add.tween(handWait).to( { y: 770 }, 10);
-        this.tenTween = this.game.add.tween(handWait).to( { x: 1200, y: 700 }, 300);
+        this.tenTween = this.game.add.tween(handWait).to( { x: 1050, y: 700 }, 300);
         this.elevenTween = this.game.add.tween(handWait).to( { y: 2000 }, 1500);
 
         this.firstTween.chain(this.secondTween);
@@ -150,6 +198,7 @@ export default class GradesState extends Phaser.State {
         this.tenTween.chain(this.elevenTween);
 
         this.stage.disableVisibilityChange = true;
+        this.next();
     }
 
     next() {
