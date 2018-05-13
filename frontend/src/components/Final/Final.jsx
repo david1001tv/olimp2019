8f5deb706e5c810079a5bdccb8a222ca5d607da9
Fruch-PC <cifrach6807@gmail.com>
2018-05-13 14:28:12 +0300

    Прикрутил лайки

--- frontend/src/components/Final/Final.jsx
+++ frontend/src/components/Final/Final.jsx
@@ -1,42 +1,66 @@
-import React, { Component } from 'react';
-import { DialogContainer } from 'react-md';
+import React, {Component} from 'react';
+import {DialogContainer} from 'react-md';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import autobind from 'autobind-decorator';
+import {getLike, toggleLike} from '~api';
+
+
 import Feedback from '../Landing/Feedback';
 
 import './Final.sass';
 
 class Final extends Component {
     state = {
-        feedbackIsVisible: false
+        feedbackIsVisible: false,
+        liked: true,
     };
 
+    
+    componentDidMount() {
+        getLike()
+            .then(res => this.setState({liked: res.userLiked}));
+    }
+
+
+    @autobind
+    handleLikeClick() {
+        this.setState({liked: !this.state.liked});
+        toggleLike();
+    }
+    
     render() {
-        const {feedbackIsVisible} = this.state;
+        const {feedbackIsVisible, liked} = this.state;
         return (
             <div>
                 <div className="wrapper">
-                <div className="final-container">
-                    <div className="content">
-                        <header>
-                            <div className="page-title">Вітаємо!</div>
-                        </header>
-                        <div className="page-subtitle">Ви пройшли квест</div>
-                        <div className="game-info">Тепер можете подивитись титри від розробників, таблицю лідерів та коментарі. Не забудьте поставити вподобайку!</div>
-                        <iframe src="https://www.youtube.com/embed/DjPRHFw4Go4?rel=0" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
-                        <div className="soc">
-                            <div className="like"></div>
-                            <div className="share">
-                                <div className="facebook"></div>
-                                <div className="twitter"></div>
-                                <div className="google"></div>
-                                <div className="linkedin"></div>
+                    <div className="final-container">
+                        <div className="content">
+                            <header>
+                                <div className="page-title">Вітаємо!</div>
+                            </header>
+                            <div className="page-subtitle">Ви пройшли квест</div>
+                            <div className="game-info">Тепер можете подивитись титри від розробників, таблицю лідерів та
+                                коментарі. Не забудьте поставити вподобайку!
                             </div>
-                        </div>
-                        <div className="leaderboard">
-                            <div className="block-title">Таблиця лідерів</div>
-                            <table cellSpacing="0" id="leaderboard">
-                                <tbody>
+                            <iframe src="https://www.youtube.com/embed/DjPRHFw4Go4?rel=0" frameBorder="0"
+                                    allow="autoplay; encrypted-media" allowFullScreen></iframe>
+                            <div className="soc">
+                                <div
+                                    className={`like ${liked && 'like--active'}`}
+                                    onClick={this.handleLikeClick}
+                                ></div>
+                                <div className="share">
+                                    <div className="facebook"></div>
+                                    <div className="twitter"></div>
+                                    <div className="google"></div>
+                                    <div className="linkedin"></div>
+                                </div>
+                            </div>
+                            <div className="leaderboard">
+                                <div className="block-title">Таблиця лідерів</div>
+                                <table cellSpacing="0" id="leaderboard">
+                                    <tbody>
                                     <tr>
                                         <th>Ім’я</th>
                                         <th>Очки</th>
@@ -52,35 +76,36 @@ class Final extends Component {
                                         <td>999999</td>
                                         <td>00:43</td>
                                     </tr>
-                                </tbody>
-                            </table>
-                            <button className="export">Експортувати в Excel</button>
+                                    </tbody>
+                                </table>
+                                <button className="export">Експортувати в Excel</button>
+                            </div>
+                            <div className="comments">
+                                <div className="block-title">Коментарі</div>
+                            </div>
                         </div>
-                        <div className="comments">
-                            <div className="block-title">Коментарі</div>
-                        </div>
-                    </div>
-                    <footer>
-                        <div className="likes">Ця гра сподобалась 543 515 користувачів.</div>
-                        <div className="copyright">Команда ДВНЗ “ПДТУ”, 2018 ©</div>
-                        <button className="btn-feedback"
-                                id="btn-feedback"
-                                onClick={() => this.setState({feedbackIsVisible: true})}>
+                        <footer>
+                            <div className="likes">Ця гра сподобалась 543 515 користувачів.</div>
+                            <div className="copyright">Команда ДВНЗ “ПДТУ”, 2018 ©</div>
+                            <button className="btn-feedback"
+                                    id="btn-feedback"
+                                    onClick={() => this.setState({feedbackIsVisible: true})}>
                                 Зворотній зв'язок
-                        </button>
-                    </footer>
+                            </button>
+                        </footer>
+                    </div>
                 </div>
-            </div>
-            <DialogContainer
+                <DialogContainer
                     focusOnMount={false}
                     visible={feedbackIsVisible}
                     onHide={() => this.setState({feedbackIsVisible: false})}
                 >
-                <Feedback />
-            </DialogContainer>
-        </div>
+                    <Feedback/>
+                </DialogContainer>
+            </div>
         );
     }
+
     export() {
 
     }
