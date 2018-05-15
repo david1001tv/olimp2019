586746803a02bb61cd9a7ccfa31c825b7ec56b76
Fruch-laptop\Fruch <cifrach6807@gmail.com>
2018-05-15 13:04:34 +0300

    Фикс

--- frontend/src/components/Game/states/ThirdStage/Labyrinth.js
+++ frontend/src/components/Game/states/ThirdStage/Labyrinth.js
@@ -1,5 +1,4 @@
 import Phaser from 'phaser';
-import PhaserInput from 'phaser-input';
 import {smartSetHeight} from '../../utils';
 
 const PLAYER_SPEED = 250;
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     cache: true,
 
     entry: {
-        vendor: ['pixi', 'p2', 'phaser', 'webfontloader', 'react', 'react-dom', 'phaser-input'],
+        vendor: ['pixi', 'p2', 'phaser', 'webfontloader', 'react', 'react-dom'],
         main: './src/index.js',
     },
     output: {
