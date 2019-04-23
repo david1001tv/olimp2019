cbfa8306b3dac3adbe5321bd9e5be096e7441776
david <d.yukhno@myrig.com>
2019-04-23 13:05:49 +0300

    updates in index sequlize

--- backend/models/sequelize.js
+++ backend/models/sequelize.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const config = require('../config');
 
 const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
+    hostname: config.hostname,
     host: config.host,
     port: config.db.port,
     dialect: 'mysql',
