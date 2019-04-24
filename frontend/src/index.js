b458c5430612f38f7f5a25f4382ebe555ba9fac2
Donny Master <donnymaster4@gmail.com>
2019-04-24 14:04:23 +0300

    коменты добавил

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,13 @@ import 'regenerator-runtime/runtime';
 import validate from 'validate.js';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from './App';
+/*
+App - в нем храниться все другие элементы и от него мы танцуем 
+*/
 
+
+import App from './App';
+// Validate.js предоставляет декларативный способ проверки объектов javascript.
 validate.options = { fullMessages: false };
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('mount-point'));
