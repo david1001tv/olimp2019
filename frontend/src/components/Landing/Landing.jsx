88db153dd6a3760f15023a32deb913c5561f2ad4
Donny Master <donnymaster4@gmail.com>
2019-04-27 12:15:23 +0300

    Главную страницу переместил в реакт

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -7,7 +7,6 @@ import photo from '../../img/photo.jpg'
 
 import Login from './Login';
 import Feedback from './Feedback';
-//import './Landing.sass';
 import './main.sass';
 import {logOut, getLikeCount} from '~api';
 
@@ -34,33 +33,63 @@ class Landing extends Component {
 
         return (
 
+    <div>
+        <div className="main">
+            <div className="main-text">
+            <div className="for_t">
+                 <h1>
+            Квест “Моя майбутня професiя”
+        </h1>
+        </div>
+       
+    </div>
+    <div className="main-text_p">
+            <div className="text_p">
+               <p>
+                    Пориньте в історію вступу до установи вищої освіти.
+                     Ваше завдання полягає у подоланні тернистого шляху 
+                     абітурієнта, який проходить вступну кампанію та знайомиться
+                      зі специфікою своєї професії!
+               </p>
+               </div>
+    </div>
+    <div className="wrapper">
+    <div className="main-button">
+        <div className="but-start">
+          
+            <a href="#"><p>ПОЧАТИ РУХ</p></a>
+        </div>
+        <div className="but-cont">
+                <a href="#"><p>ПРОДОВЖИТИ</p></a>
+        </div>
+    </div>
 
 
-             <div>
-                <div className="wrapper">
-                    <div className="content">
-                        <header>
-                            <div className ="page-title">Квест “Абітурієнт”</div>
-                        </header>
-                        <div className="page-subtitle">Вітаємо вас на нашому учбово-розважальному порталі!</div>
-                        <div className="game-info">Пориньте в історію вступу до установи вищої освіти. Ваше завдання
-                            полягає у подоланні тернистого шляху абітурієнта, який проходить вступну кампанію та
-                            знайомиться зі специфікою своєї професії!
-                        </div>
-                        <div className="buttons">
-                            <Link className="btn btn-start" to="/game" onClick={() => logOut()}/>
-                            <button
-                                className="btn btn-continue"
-                                onClick={() => this.setState({formIsVisible: true})}
-                            />
-                        </div>
-                    </div>
-                    <footer>
-                      
-                    </footer>
-                </div>
-             
-            </div>
+
+</div>
+
+ <div className="footer">
+    <div className="butf">
+        <div className="wrap">
+        <div className="link-f"><div> <p> Правила Квесту</p></div></div>
+        <div className="link-f"><div><p> КОМАНДА ДВНЗ “ПДТУ”, 2019 ©</p></div></div>
+        <div className="link-f"><div  onClick={() => this.setState({feedbackIsVisible: true})}><p> Зворотній зв'язок</p></div></div>
+        <div className="link-f"> <div><p> ОФІЦІЙНИЙ САЙТ ПДТУ</p></div></div>
+    </div>
+    </div>
+</div>
+    </div>
+
+    <DialogContainer
+        focusOnMount={false}
+        visible={feedbackIsVisible}
+        onHide={() => this.setState({feedbackIsVisible: false})}
+    >
+        <Feedback />
+    </DialogContainer>
+
+</div>
+
         );
              
     }
